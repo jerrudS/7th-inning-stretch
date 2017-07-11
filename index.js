7c8b020dd9444a8829a31da0e69e0e9293d6a934
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-07-11 15:47:18 -0700

    Create an express app with a route to handle requests for events.  Link together "public" folder and render index.html for get at root. #1

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
+const authentication = require('./authentication')
 const request = require('request')
-// const app = requre('express')
-// const fs = require('fs')
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const app = express()
 
-request
-  .get('https://api.seatgeek.com/2/events?client_id=Nzg1MDAxMnwxNDk5NTM3NDE4LjI&client_secret=a97cb08ed89da0d571c2d0be476d11d1899c39ee779f71f8539907d277d7b532&performers[home_team].id=8')
-  .on('response', function (response) {
-    console.log(response.statusCode)
-    console.log(response.headers['content-type'])
-    const data = JSON.parse(response)
-    console.log(data)
+const username = authentication.client_id
+const password = authentication.client_secret
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
+
+// function requestGeek(url) {
+//   request
+//     .get(url)
+//     .on('response', response => {
+//       console.log(response.statusCode)
+//       console.log(response.headers['content-type'])
+//     })
+//   return request
+// }
+// requestGeek('https://api.seatgeek.com/2/events?performers.slug=new-york-mets' + '&client_id=' + username + '&client_secret=' + password)
+function mapEvent(team) {
+  const revisedEvent = team.map(item => {
+    return {
+      Rating: item.score,
+      Matchup: item.title,
+      Date: item.datetime_local,
+      Lowest_Ticket_Price: item.stats.lowest_price,
+      Average_Ticket_Price: item.stats.average_price
+    }
   })
+  return revisedEvent
+}
+
+app.get('/events/new-york-mets', (req, res) => {
+  request('https://api.seatgeek.com/2/events?performers.slug=new-york-mets' + '&client_id=' + username + '&client_secret=' + password, (error, response, body) => {
+    console.log('error:', error)
+    console.log('statusCode:', response && response.statusCode)
+    mapEvent(JSON.parse(body).events)
+    res.send(mapEvent(JSON.parse(body).events))
+  })
+})
+
+app.listen(3004, () => {
+  console.log('Listening on port 3004')
+})
