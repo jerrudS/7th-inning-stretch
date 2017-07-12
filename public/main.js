a6f4f07a3a306911f9fe212b50f9ff2e7194ce7d
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-07-12 12:29:02 -0700

    Define a function for rendering a table. #1

--- public/main.js
+++ public/main.js
@@ -5,8 +5,42 @@ function expressData(path) {
 }
 expressData('/events/new-york-mets')
 
+function renderTable() {
+  const tableDiv = document.querySelector('#table')
+  const table = document.createElement('table')
+  const head = document.createElement('thead')
+  const tableRow = document.createElement('tr')
+  for (let i = 0; i < 7; i++) {
+    const header = document.createElement('th')
+    tableRow.appendChild(header)
+    header.textContent = 'Header'
+  }
+  table.setAttribute('class', 'ui celled table')
+
+  tableDiv.appendChild(table)
+  table.appendChild(head)
+  head.appendChild(tableRow)
+
+  const tableBody = document.createElement('tbody')
+  table.appendChild(tableBody)
+
+  function createRow() {
+    const tableRow2 = document.createElement('tr')
+    for (let j = 0; j < 7; j++) {
+      const tableData = document.createElement('td')
+      tableData.textContent = 'Cell'
+      tableRow2.appendChild(tableData)
+    }
+    tableBody.appendChild(tableRow2)
+  }
+  for (let x = 0; x < 15; x++) {
+    createRow()
+  }
+}
+renderTable()
+
 const dropdown = document.querySelector('#dropdown')
 
-dropdown.addEventListener('change', function () {
+dropdown.addEventListener('change', () => {
   console.log('changing')
 })
